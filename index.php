b8ad3172ad74845a9c464755dbce383dbba13409
siddharth <163295466+chiku2312@users.noreply.github.com>
2025-11-03 14:09:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Hello from AWS Elastic Beanstalk PHP App íº€</h1>";
+echo "<h1>Hello from AWS Elastic Beanstalk PHP php App ï¿½ï¿½ï¿½</h1>";
 echo "<p>Deployed successfully using GitHub Actions / Console!</p>";
 ?>
 
